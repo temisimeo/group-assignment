58f6f02912aa522e6317c36254f989954aaedb36
Allyvanny <altodezdel@gmail.com>
2025-05-20 16:09:49 +0300

    Added the contact form for collecting contact information for a student

--- index.php
+++ index.php
@@ -23,6 +23,8 @@ if(isset($_POST['save'])){
 <input type="text" name="talent_title" class="form-control w-75" placeholder="Enter your talent/innovation title"><br>
 <label for="">Talent/Innovation Description</label><br>
 <textarea name="description" class="form-control w-75" id="" placeholder="Tell more about your talent/innovation" required></textarea><br>
+<label for="">Contacts</label><br>
+<input type="text" name="contacts" class="form-control w-75" placeholder="Enter your contacts"><br>
 <button type="submit" class="btn btn-primary" name="save">Submit</button>
 <a href="student_talent.php" class="btn btn-success">View Student talents</a>
    </form>
