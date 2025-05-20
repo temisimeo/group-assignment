599aa21239fc6f0a98faf22d228c4d82a7a4832e
Allyvanny <altodezdel@gmail.com>
2025-05-20 20:43:51 +0300

    Updataed the delete.php

--- delete.php
+++ delete.php
@@ -5,7 +5,7 @@ if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
     $sql = "DELETE FROM talents WHERE id = $id";
     if (mysqli_query($con, $sql)) {
-        header("Location: description.php"); 
+        header("Location: student_talent.php"); 
         exit();
     } else {
         echo "Error deleting record: " . mysqli_error($con);
