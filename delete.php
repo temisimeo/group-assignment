2c1dab85de33060bc9f805b4af674ed121d60092
Allyvanny <altodezdel@gmail.com>
2025-05-20 16:48:24 +0300

    Added delete.php for deleting talent records

---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php
+include 'database.php';
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $sql = "DELETE FROM talents WHERE id = $id";
+    if (mysqli_query($con, $sql)) {
+        header("Location: description.php"); 
+        exit();
+    } else {
+        echo "Error deleting record: " . mysqli_error($con);
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
