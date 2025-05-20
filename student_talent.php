2dc4d856da0d5084600745e5cf18786edd1622f2
Allyvanny <altodezdel@gmail.com>
2025-05-20 21:19:29 +0300

    Added ability to view the contacts on the dashbord for student talent show case

--- student_talent.php
+++ student_talent.php
@@ -7,10 +7,12 @@ while($see=mysqli_fetch_array($take)){
      $gname=$see['name'];
      $gtal=$see['talent_title'];
      $gdes=substr($see['description'], 0, 30);
+     $contact=$see['contact'];
      $view.="
                <p><strong>Student name:</strong> $gname</p>
                <p><i>Title: </i> $gtal</p>
                <p><strong>Description: </strong>$gdes <strong><a href='description.php?id=$id' class='text-primary' style='text-decoration: none; font-size: 20px;'>Read description</a></strong></p>
+               <p><strong>Contact:</strong> $contact</p>
                <p>
                <a href='edit.php?id=$id' class='btn btn-sm btn-primary'>Edit</a>
                <a href='delete.php?id=$id' class='btn btn-sm btn-danger' onclick=\"return confirm('Are you sure you want to delete this record?');\">Delete</a>
